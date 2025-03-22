5a2cdcdff04b77012564b58756ecdd8155b9f59b
Juliana Gentile <jlg3012@g.rit.edu>
2025-03-21 23:16:04 -0400

    Final part 4 implementation

--- Lab6-DCMotor.c
+++ Lab6-DCMotor.c
@@ -27,6 +27,7 @@ void delay(int del){
 int main(void) {
 	// Initialize UART and PWM
 	// INSERT CODE HERE
+	uint16_t period = SystemCoreClock / 10000;
 	uart0_init();
 
 	// Print welcome over serial
@@ -35,23 +36,27 @@ int main(void) {
 	/* Part 1 - UNCOMMENT THIS
 	// Generate 20% duty cycle at 10kHz
 	// INSERT CODE HERE */
-	TIMER_A2_PWM_Init(50, 0.075, 1); // 50Hz frequency, 7.5% duty cycle (1.5ms pulse width)
+	TIMER_A0_PWM_Init(period, 0.2, 1);
+	TIMER_A0_PWM_Init(period , 10, 4);
+	TIMER_A0_PWM_Init(period , 0.2, 2);
+	TIMER_A0_PWM_Init(period , 10, 3);
+	TIMER_A2_PWM_Init(50, (1-0.075), 1); // 50Hz frequency, 7.5% duty cycle (1.5ms pulse width)
 
 	for(;;)  //loop forever
 	{
 		// Change the pulse width back to 1.5ms (7.5% duty cycle)
-		TIMER_A2_PWM_DutyCycle(0.075, 1);
-		delay(1000);
+		//TIMER_A2_PWM_DutyCycle(1-0.075, 1);
+		//delay(1);
 
 		// Uncomment below for second signoff
 		
 		// Change the pulse width to 2ms (10% duty cycle)
-		// TIMER_A2_PWM_DutyCycle(0.10, 1);
-		// delay(1000);
+		//TIMER_A2_PWM_DutyCycle(1-0.1, 1);
+		//delay(1);
 
 		// Change the pulse width to 1ms (5% duty cycle)
-		// TIMER_A2_PWM_DutyCycle(0.05, 1);
-		// delay(1000);
+		//TIMER_A2_PWM_DutyCycle(1-0.05, 1);
+		//delay(1);
 	}
 
 	return 0;
